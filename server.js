3e8c279f902b02b449fbbffad7815f47a6d41e99
vimpoli <vimprakasholi@gmail.com>
2025-07-15 09:12:44 +0545

    Delete existing fiels

--- server.js
+++
@@ -1,9 +0,0 @@
-const http = require("http");
-
-const server = http.createServer((request, response) => {
-    response.end("Hello Mohan");
-});
-
-server.listen(5000, () => {
-    console.log("Server running at port 5000...");
-});
